41e7e70102723530bcdc9566e818a3eab04b02e2
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-01-30 14:32:14 +0300

    minor update

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,49 @@
 #!/usr/bin/python3
-"""Defines a base class module"""
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
 from datetime import datetime
-import uuid
 
 
-class BaseModel():
-    """this is base class parent class which contain
-       id, created at and updated at    
-    """
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """initialize a BaseClass with public instances"""
-        if kwargs != {}:
-            for key in kwargs:
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.fromisoformat(kwargs[key])
-                elif key == "__class__":
-                    pass
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
-        """Update Updated at when it's instances are being updated."""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        data = {}
-        for key in self.__dict__:
-            if key == 'created_at' or key == 'updated_at':
-                data[key] = self.__dict__[key].isoformat()
-            else:
-                data[key] = self.__dict__[key]
-        data["__class__"] = self.__class__.__name__
-        return data
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
     def __str__(self):
-        """Define the Printable representation of BaseClass."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
